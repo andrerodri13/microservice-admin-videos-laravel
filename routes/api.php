ca793f778a08e8a5bd6b26eb567e4e7290eb3fc1
André S. Rodrigues <andrerodri13@hotmail.com>
2023-04-07 21:59:22 -0300

    Logs completo

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
 use App\Http\Controllers\Api\{CastMemberController, CategoryController, GenreController, VideoController};
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
+
+Route::get('test', function (){
+   Log::warning('test');
+});
 
 Route::middleware(['auth:api', 'can:admin-catalog'])->group(function () {
     Route::apiResource('/videos', VideoController::class);
